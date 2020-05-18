531c51c7b1486dac2e2a5f01571c42fbad26dda8
bastin <giselebastin.web@gmail.com>
2020-05-18 15:49:40 +0200

    200518_avtPartials
    avant partials

---
+++ app/config/init.php
@@ -0,0 +1,7 @@
+<?php
+
+/*
+./noyau/init.php
+*/
+require_once '../app/config/parametres.php';
+require_one '../noyau/connexion.php';
---
+++ app/config/parametres.php
@@ -0,0 +1,14 @@
+<?php
+/*
+  ./app/config/parametres.php
+ */
+
+// PARAMETRES DE CONNEXION A LA DATABASE
+    define('HOSTNAME', 'localhost:3306');
+    define('DBNAME',   'readit_2020');
+    define('USERNAME', 'root');
+    define('USERPWD',  '');
+
+// INITIALISATION DES ZONES DYNAMIQUES DU TEMPLATE
+    $title   = '';
+    $content = '';
